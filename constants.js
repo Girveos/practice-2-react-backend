c37f584efbb4bec1ea966aea8a504fbd4385fe5f
AlejandroLedesma29 <ledesmazja@gmail.com>
2023-10-26 17:12:26 -0500

    Finish auth and middlewares

---
+++ constants.js
@@ -0,0 +1,7 @@
+const JWT_SECRET_KEY = "acceso-sistema-software-2-2023"
+const API_VERSION = "v1";
+
+module.exports = {
+    JWT_SECRET_KEY, 
+    API_VERSION
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const user_routes_access = require("./userRoutes"); 
+const auth_routes_access = require("./authRoutes");
 const routes = express. Router();
 const routes_system = (app) => {
     /* http://localhost:5000/api/v1 */
     app.use("/api/v1", routes);
     routes.use("/user", user_routes_access);
+    routes.use("/", auth_routes_access);
 };
 
 module.exports = routes_system;
